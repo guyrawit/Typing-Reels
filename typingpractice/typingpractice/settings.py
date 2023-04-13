f1e3e924cb1b0395e4bc469412a4d78020bba252
Rawit Lertluksanaporn <6570201021@student.chula.ac.th>
2023-04-13 21:53:31 +0700

    update random get movie (start view)

--- typingpractice/typingpractice/settings.py
+++ typingpractice/typingpractice/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'typingtest' , 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'typingtest' , 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- typingpractice/typingtest/views.py
+++ typingpractice/typingtest/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .forms import TypingForm
-
+from .models import MovieScene
 
 def index(request):
     return render(request, 'typingtest/index.html')
 
 def start(request):
+    random_movie = MovieScene.objects.order_by('?').first()
+    print(random_movie.title)
     form = TypingForm()
-    context = {'form': form}
+    context = {'form': form, "movie": random_movie}
     return render(request, 'typingtest/start.html', context)
 
 # def typing_view(request):
